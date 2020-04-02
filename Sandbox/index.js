4a6cf2837ee958c4a3642ecc49e2f95a0c4eb55b
vt5496 <tkk.vlad@gmail.com>
2020-04-02 20:57:15 +0300

    Third step

---
+++ Sandbox/index.js
@@ -0,0 +1,18 @@
+
+
+/*
+function понятьРекурсию(i){
+    if (i === 1||i===2) {
+        return 1
+    }
+    return понятьРекурсию(i-2)+понятьРекурсию(i-1);
+}
+console.log(понятьРекурсию(10));*/
+
+let kek = [1, 2, 3];
+function massiv(...lol) {
+return(lol);
+}
+console.log(massiv(4, 5, 6));
+let cheburek = kek.concat(massiv(4, 5, 6);
+console.log(cheburek);
